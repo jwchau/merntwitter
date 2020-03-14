613e75d8cba5abe5e2af9d3618f4d2e75f0267e1
John C <john.win.chau@gmail.com>
2020-03-14 16:03:32 -0700

    add user and tweet routes/schema/models/authentication

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //express init
 const express = require('express');
+
+//invoke express
 const app = express();
+
+//routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 //mongoose init
 const mongoose = require('mongoose');
@@ -10,6 +16,10 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+//passport (jwt strategy token thing)
+const passport = require('passport');
+require('./config/passport')(passport);
+
 //bodyParser for json
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,14 +28,13 @@ app.use(bodyParser.json());
 //which port to run on
 const port = process.env.PORT || 5000;
 
-//routes
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
-
-//response
-app.get("/", (req, res) => res.send("Hello World!"));
+//use routes
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+//response
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.use(passport.initialize());
 
 //open socket
 app.listen(port, () => console.log(`Server is running on port ${port}`));
