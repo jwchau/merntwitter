14c80a6d2aff340938d4c709b1dabffb7615efb7
John C <john.win.chau@gmail.com>
2020-03-14 11:28:46 -0700

    mongodb connect setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //initialization
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 //which port to run on
 const port = process.env.PORT || 5000;
