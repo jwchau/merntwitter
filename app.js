9fd92a2e15580e02db3e555b45ea1b26072798b4
John C <john.win.chau@gmail.com>
2020-03-14 23:40:38 -0700

    heroku setup, part 1

--- app.js
+++ app.js
@@ -40,3 +40,12 @@ app.use(passport.initialize());
 
 //open socket
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
